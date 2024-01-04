afc826fe35ddd873ca10cffadd4f2e5a3712d669
uwol-is-june <std06158@naver.com>
2024-01-04 18:05:16 +0900

    1.4 commit

--- Source/LWCastle/Private/Uwol/uwol_test.cpp
+++ Source/LWCastle/Private/Uwol/uwol_test.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Uwol/uwol_test.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
 
 // Sets default values
 Auwol_test::Auwol_test()
@@ -9,6 +11,21 @@ Auwol_test::Auwol_test()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Mesh Load and set location, rotation
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/playermodel/Nobeta_mesh.Nobeta_mesh'"));
+	if (TempMesh.Succeeded()) {
+		GetMesh()->SetSkeletalMesh(TempMesh.Object);
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -90), FRotator(0, -90, 0));
+	}
+
+	// Set Camera
+	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	springArmComp->SetupAttachment(RootComponent);
+	springArmComp->SetRelativeLocation(FVector(0, 70, 90));
+	springArmComp->TargetArmLength = 400;
+
+	tpsCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("TpsCamComp"));
+	tpsCamComp->SetupAttachment(springArmComp);
 }
 
 // Called when the game starts or when spawned
