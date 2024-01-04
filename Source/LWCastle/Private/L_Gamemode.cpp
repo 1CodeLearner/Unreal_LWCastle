959732e059817e9fa41e72d2b6754aa7bd859a22
uwol-is-june <std06158@naver.com>
2024-01-04 16:01:21 +0900

    default log system

---
+++ Source/LWCastle/Private/L_Gamemode.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "L_Gamemode.h"
+#include "LWCastle/LWCastle.h"
+
+AL_Gamemode::AL_Gamemode()
+{
+	PRINT_LOG(TEXT("My Log : %s"), TEXT("Lttle Witch in the Castle"));
+
+}
+
---
+++ Source/LWCastle/Public/L_Gamemode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "L_Gamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LWCASTLE_API AL_Gamemode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AL_Gamemode();
+	
+};
