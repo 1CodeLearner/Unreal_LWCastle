a1044b90e08434166960cb69f645fc9eebd1ff24
uwol-is-june <std06158@naver.com>
2024-01-11 17:20:42 +0900

    c

--- Source/LWCastle/Private/Uwol/uwol_test.cpp
+++ Source/LWCastle/Private/Uwol/uwol_test.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "Uwol/DefaultMagic.h"
 #include "Uwol/uwol_test.h"
+#include "Uwol/DefaultMagic.h"
 #include <Blueprint/UserWidget.h>
 #include <Camera/CameraComponent.h>
 #include <GameFramework/SpringArmComponent.h>
