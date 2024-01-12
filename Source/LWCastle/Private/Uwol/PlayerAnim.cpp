1acf85dce41a32043dced07a698f08cc940ac994
uwol-is-june <std06158@naver.com>
2024-01-12 18:30:49 +0900

    1.12 commit

---
+++ Source/LWCastle/Private/Uwol/PlayerAnim.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Uwol/PlayerAnim.h"
+#include "Uwol/uwol_test.h"
+#include <GameFramework/CharacterMovementComponent.h>
+
+void UPlayerAnim::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	auto ownerPawn = TryGetPawnOwner();
+	auto player = Cast<Auwol_test>(ownerPawn);
+
+	if (player)
+	{
+		FVector velocity = player->GetVelocity();
+		FVector forwardVector = player->GetActorForwardVector();
+		speed = FVector::DotProduct(forwardVector, velocity);
+
+		// 좌우 속도
+		FVector rightVector = player->GetActorRightVector();
+		verdirection = FVector::DotProduct(rightVector, velocity);
+
+		// 공중
+		auto movement = player->GetCharacterMovement();
+		isInAir = movement->IsFalling();
+	}
+
+
+
+}
+
+void UPlayerAnim::PlayAttackAnim()
+{
+	Montage_Play(attackAnimMontage);
+}
+
---
+++ Source/LWCastle/Public/Uwol/PlayerAnim.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LWCASTLE_API UPlayerAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = PlayerAnim)
+	float speed = 0;
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	// 공중 체크
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=PlayerAnim)
+	bool isInAir = false;
+
+	// 좌우 이동속도
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=PlayerAnim)
+	float verdirection = 0;
+
+	// 공격 재생 몽타주
+	UPROPERTY(EditDefaultsOnly, Category=PlayerAnim)
+	class UAnimMontage* attackAnimMontage;
+
+	// 애니메이션 재생
+	void PlayAttackAnim();
+
+
+};
