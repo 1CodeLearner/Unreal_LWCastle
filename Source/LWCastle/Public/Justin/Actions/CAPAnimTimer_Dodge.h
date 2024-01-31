68091790f87fb8d84d8ae7d645a74fd54886b171
1CodeLearner <51289481+1CodeLearner@users.noreply.github.com>
2024-01-31 18:56:38 +0900

    Saving first

---
+++ Source/LWCastle/Public/Justin/Actions/CAPAnimTimer_Dodge.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Justin/Actions/CAPAnimTimer.h"
+#include "CAPAnimTimer_Dodge.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class LWCASTLE_API UCAPAnimTimer_Dodge : public UCAPAnimTimer
+{
+	GENERATED_BODY()
+public:
+	virtual void StartAction_Implementation()
+};
